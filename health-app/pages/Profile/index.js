6a66d54b7b0e1e41ea45db77fb4840e87488c785
OppaiMilk <jiayi.seur@gmail.com>
2023-10-15 10:23:18 +0800

    add

---
+++ health-app/pages/Profile/index.js
@@ -0,0 +1,341 @@
+import styles from './index.module.css';
+
+
+const Marketplace = () => {
+  	return (
+    		<div className={styles.marketplace}>
+      			<div className={styles.headersV5}>
+        				<div className={styles.container2} />
+        				<div className={styles.sections}>
+          					<div className={styles.home}>Home</div>
+          					<div className={styles.linkList}>
+            						<div className={styles.home}>Services</div>
+            						<img className={styles.vectorIcon5} alt="" src="Vector.svg" />
+          					</div>
+          					<div className={styles.linkList}>
+            						<div className={styles.home}>Marketplace</div>
+            						<img className={styles.vectorIcon5} alt="" src="Vector.svg" />
+          					</div>
+          					<div className={styles.home}>Contact</div>
+        				</div>
+        				<div className={styles.buttonsSet}>
+          					<div className={styles.masterPrimaryButton1}>
+            						<b className={styles.home}>Connect Wallet</b>
+          					</div>
+          					<img className={styles.userIcon1} alt="" src="User.svg" />
+        				</div>
+        				<div className={styles.frameParent}>
+          					<div className={styles.group5CopyParent}>
+            						<img className={styles.group5Copy2} alt="" src="Group 5 Copy.svg" />
+            						<div className={styles.healthParent}>
+              							<div className={styles.health}>Health</div>
+              							<div className={styles.chain}>Chain</div>
+            						</div>
+          					</div>
+          					<div className={styles.pro}>Pro</div>
+        				</div>
+      			</div>
+      			<div className={styles.containerParent}>
+        				<div className={styles.container3}>
+          					<div className={styles.content}>
+            						<img className={styles.contentChild} alt="" src="Group 1890.png" />
+            						<div className={styles.text9}>
+              							<div className={styles.tag}>
+                								<b className={styles.pricing}>Sign Up Today</b>
+              							</div>
+              							<div className={styles.bookAFree1}>Book A Free Consultation.</div>
+              							<div className={styles.buttonPrimaryIcon3}>
+                								<div className={styles.content1}>
+                  									<div className={styles.text10}>Download The App</div>
+                  									<img className={styles.solidArrowRightSm1} alt="" src="Solid arrow right sm.svg" />
+                								</div>
+              							</div>
+            						</div>
+          					</div>
+        				</div>
+        				<div className={styles.frameGroup}>
+          					<div className={styles.frameDiv}>
+            						<div className={styles.group5CopyParent}>
+              							<img className={styles.group5Copy2} alt="" src="Group 5 Copy.svg" />
+              							<div className={styles.healthParent}>
+                								<div className={styles.health}>Health</div>
+                								<div className={styles.chain}>Chain</div>
+              							</div>
+            						</div>
+            						<div className={styles.pro}>Pro</div>
+          					</div>
+          					<div className={styles.footer}>
+            						<div className={styles.aboutParent}>
+              							<b className={styles.about}>About</b>
+              							<div className={styles.aboutUs}>About Us</div>
+              							<div className={styles.privacyPolicy}>Privacy Policy</div>
+              							<div className={styles.termsOfService1}>Terms of Service</div>
+            						</div>
+            						<div className={styles.quickLinksParent}>
+              							<b className={styles.about}>Quick Links</b>
+              							<div className={styles.findADoctor1}>Find a Doctor</div>
+              							<div className={styles.bookAnAppointment1}>Book an Appointment</div>
+              							<div className={styles.telehealthServices}>Telehealth Services</div>
+              							<div className={styles.medicationOrdering}>Medication Ordering</div>
+            						</div>
+            						<div className={styles.aCuttingEdgeHealthcareTechWrapper}>
+              							<div className={styles.aCuttingEdgeHealthcare1}>A cutting-edge healthcare technology company dedicated to revolutionizing the healthcare industry through the power of blockchain.</div>
+            						</div>
+            						<div className={styles.contactParent}>
+              							<b className={styles.contact1}>Contact</b>
+              							<div className={styles.div}>+012 3456789</div>
+              							<div className={styles.healthchainprogmailcom}>healthchainpro@gmail.com</div>
+              							<div className={styles.youtubeParent}>
+                								<img className={styles.youtubeIcon1} alt="" src="youtube.svg" />
+                								<img className={styles.youtubeIcon1} alt="" src="discord.svg" />
+                								<img className={styles.youtubeIcon1} alt="" src="instagram.svg" />
+              							</div>
+            						</div>
+          					</div>
+        				</div>
+        				<div className={styles.copyright20231}>Copyright © 2023 HealthChainPro. All rights reserved.</div>
+      			</div>
+      			<img className={styles.vectorIcon7} alt="" src="Vector.svg" />
+      			<div className={styles.ourCoreFeatures2}>Patient Details</div>
+      			<div className={styles.ourCoreFeatures3}>Wearable Device</div>
+      			<div className={styles.frame}>
+        				<div className={styles.frameParent1}>
+          					<div className={styles.frameParent2}>
+            						<div className={styles.istockphoto1330046035612x612Wrapper}>
+              							<img className={styles.istockphoto1330046035612x612Icon} alt="" src="istockphoto-1330046035-612x612 1.png" />
+            						</div>
+            						<div className={styles.buttonPrimaryIconParent}>
+              							<div className={styles.buttonPrimaryIcon4} />
+              							<div className={styles.vectorParent}>
+                								<img className={styles.vectorIcon8} alt="" src="Vector.svg" />
+                								<img className={styles.socialIcons2} alt="" src="Social Icons.svg" />
+                								<img className={styles.socialIcons3} alt="" src="Social Icons.svg" />
+              							</div>
+            						</div>
+          					</div>
+          					<div className={styles.frameParent3}>
+            						<div className={styles.frameParent4}>
+              							<div className={styles.frameWrapper}>
+                								<div className={styles.frameWrapper}>
+                  									<div className={styles.frameWrapper}>
+                    										<div className={styles.oslerSAiDrivenTelehealthBParent}>
+                      											<div className={styles.oslerSAi}>Anziano Tier</div>
+                      											<div className={styles.pricing1}>Senior Engineer  | Kuala Lumpur, Malaysia</div>
+                    										</div>
+                  									</div>
+                								</div>
+              							</div>
+              							<div className={styles.buttonPrimaryIcon3}>
+                								<div className={styles.content2}>
+                  									<div className={styles.text10}>Claim Rewards</div>
+                								</div>
+              							</div>
+            						</div>
+            						<div className={styles.frameChild} />
+            						<div className={styles.frameParent5}>
+              							<div className={styles.basicInformationWrapper}>
+                								<div className={styles.basicInformation}>Basic Information</div>
+              							</div>
+              							<div className={styles.frameParent6}>
+                								<div className={styles.iconCalendarParent}>
+                  									<img className={styles.message} alt="" src="Icon_Calendar.svg" />
+                  									<div className={styles.male}>January 15, 1980</div>
+                								</div>
+                								<div className={styles.iconCalendarParent}>
+                  									<img className={styles.message} alt="" src="User_alt.svg" />
+                  									<div className={styles.male}>Male</div>
+                								</div>
+                								<div className={styles.iconCalendarParent}>
+                  									<div className={styles.message}>
+                    										<div className={styles.messageChild} />
+                    										<img className={styles.messageItem} alt="" src="Vector 3.svg" />
+                  									</div>
+                  									<div className={styles.male}>anizianotier@email.com</div>
+                								</div>
+                								<div className={styles.iconCalendarParent}>
+                  									<img className={styles.message} alt="" src="Phone.svg" />
+                  									<div className={styles.male}>(123) 456-7890</div>
+                								</div>
+                								<div className={styles.iconLocationParent}>
+                  									<img className={styles.message} alt="" src="Icon_Location.svg" />
+                  									<div className={styles.kualaLumpurMalaysia1}>Kuala Lumpur, Malaysia</div>
+                								</div>
+              							</div>
+            						</div>
+          					</div>
+        				</div>
+        				<div className={styles.content3}>
+          					<div className={styles.frame1}>
+            						<div className={styles.logomark}>
+              							<div className={styles.logoPillow4} />
+              							<div className={styles.logoPillow5} />
+              							<div className={styles.logoPillow6} />
+              							<div className={styles.logoPillow7} />
+            						</div>
+          					</div>
+          					<div className={styles.text12}>
+            						<div className={styles.frame2}>
+              							<div className={styles.frame3}>
+                								<div className={styles.jun}>sEPT 9, 2023</div>
+                								<div className={styles.oslerPillow1} />
+                								<div className={styles.jun}>john smith</div>
+              							</div>
+              							<div className={styles.oslerSAi1}>Patient Data Privacy and</div>
+              							<div className={styles.frame4}>
+                								<div className={styles.tag1}>
+                  									<b className={styles.pricing}>AI</b>
+                								</div>
+                								<div className={styles.tag2}>
+                  									<b className={styles.pricing3}>Machine Learning</b>
+                								</div>
+              							</div>
+            						</div>
+          					</div>
+        				</div>
+      			</div>
+      			<div className={styles.frameParent7}>
+        				<div className={styles.frameParent8}>
+          					<div className={styles.groupParent}>
+            						<div className={styles.ellipseParent}>
+              							<div className={styles.groupChild} />
+              							<img className={styles.calendarWithChecklistDateSIcon2} alt="" src="calendar-with-checklist-date-schedule-3d-icon 1.png" />
+            						</div>
+            						<b className={styles.upcomingAppointments}>Upcoming Appointments</b>
+          					</div>
+          					<div className={styles.frameWrapper2}>
+            						<div className={styles.frameParent9}>
+              							<div className={styles.istockphoto1330046035612x612Parent}>
+                								<img className={styles.istockphoto1330046035612x612Icon1} alt="" src="istockphoto-1330046035-612x612 1.png" />
+                								<div className={styles.todayParent}>
+                  									<b className={styles.today}>Today !</b>
+                  									<b className={styles.upcomingAppointments}>Dr. Linda Walker</b>
+                  									<div className={styles.upcomingAppointments}>Video Consultancy</div>
+                								</div>
+              							</div>
+              							<div className={styles.frameItem} />
+            						</div>
+          					</div>
+          					<div className={styles.frameWrapper2}>
+            						<div className={styles.frameParent9}>
+              							<div className={styles.istockphoto1330046035612x612Parent}>
+                								<div className={styles.image13Wrapper}>
+                  									<img className={styles.image13Icon} alt="" src="image 13.png" />
+                								</div>
+                								<div className={styles.todayParent}>
+                  									<b className={styles.thNov20231}>26th Nov 2023</b>
+                  									<b className={styles.upcomingAppointments}>Dr. Linda Walker</b>
+                  									<div className={styles.upcomingAppointments}>Physical Consultancy</div>
+                								</div>
+              							</div>
+              							<div className={styles.frameInner} />
+            						</div>
+          					</div>
+        				</div>
+        				<div className={styles.frameParent12}>
+          					<div className={styles.groupParent1}>
+            						<div className={styles.ellipseParent}>
+              							<div className={styles.groupChild} />
+              							<img className={styles.calendarWithChecklistDateSIcon2} alt="" src="calendar-with-checklist-date-schedule-3d-icon 1.png" />
+            						</div>
+            						<b className={styles.upcomingAppointments}>Medicine Collection</b>
+          					</div>
+          					<div className={styles.frameWrapper4}>
+            						<div className={styles.frameParent9}>
+              							<div className={styles.istockphoto1330046035612x612Parent}>
+                								<img className={styles.abstractBlurDefocusedPharmaIcon} alt="" src="abstract-blur-defocused-pharmacy-drug-store 1.png" />
+                								<div className={styles.frameParent14}>
+                  									<div className={styles.yomaPharmacyParent}>
+                    										<b className={styles.upcomingAppointments}>Yoma Pharmacy</b>
+                    										<div className={styles.upcomingAppointments}>by 27 th July</div>
+                  									</div>
+                  									<img className={styles.qrCodeForMobileEnglishWikIcon} alt="" src="QR_code_for_mobile_English_Wikipedia 1.svg" />
+                								</div>
+              							</div>
+              							<div className={styles.frameItem} />
+            						</div>
+          					</div>
+        				</div>
+      			</div>
+      			<div className={styles.healthOverviewParent}>
+        				<b className={styles.healthOverview}>Health Overview</b>
+        				<div className={styles.frameWrapper5}>
+          					<div className={styles.bloodSugarParent}>
+            						<div className={styles.bloodSugar}>
+              							<div className={styles.bloodSugarChild} />
+              							<div className={styles.steps}>Steps</div>
+              							<b className={styles.steps1}>steps</b>
+              							<div className={styles.div2}>1873</div>
+              							<img className={styles.bloodSugarItem} alt="" src="Group 11.svg" />
+              							<img className={styles.bloodSugarInner} alt="" src="Group 31.svg" />
+              							<div className={styles.normalWrapper}>
+                								<div className={styles.normal}>Normal</div>
+              							</div>
+            						</div>
+            						<div className={styles.bloodSugar}>
+              							<div className={styles.bloodSugarChild} />
+              							<div className={styles.steps}>Blood Sugar</div>
+              							<b className={styles.mgDl}>mg / dL</b>
+              							<div className={styles.div2}>80</div>
+              							<img className={styles.bloodSugarItem} alt="" src="Group 11.svg" />
+              							<img className={styles.rectangleParent} alt="" src="Group 31.svg" />
+              							<div className={styles.normalContainer}>
+                								<div className={styles.normal}>Normal</div>
+              							</div>
+            						</div>
+            						<div className={styles.bloodSugar}>
+              							<div className={styles.bloodSugarChild} />
+              							<div className={styles.bloodPressure1}>Blood Pressure</div>
+              							<div className={styles.div2}>102</div>
+              							<b className={styles.b}>/ 72</b>
+              							<b className={styles.mmhg}>mmhg</b>
+              							<img className={styles.bloodPressureItem} alt="" src="Group 30.svg" />
+              							<div className={styles.rectangleParent}>
+                								<div className={styles.groupInner} />
+                								<img className={styles.nounBloodPressure1098911} alt="" src="noun_Blood Pressure_10989 1.svg" />
+              							</div>
+              							<div className={styles.normalFrame}>
+                								<div className={styles.normal}>Normal</div>
+              							</div>
+            						</div>
+            						<div className={styles.bloodSugar}>
+              							<div className={styles.bloodSugarChild} />
+              							<div className={styles.steps}>Heart Rate</div>
+              							<div className={styles.div2}>98</div>
+              							<b className={styles.mgDl}>bpm</b>
+              							<div className={styles.rectangleParent}>
+                								<div className={styles.groupChild1} />
+                								<img className={styles.groupIcon2} alt="" src="Group.svg" />
+              							</div>
+              							<div className={styles.normalWrapper1}>
+                								<div className={styles.normal}>Normal</div>
+              							</div>
+              							<img className={styles.heartRateItem} alt="" src="Group 29.svg" />
+            						</div>
+          					</div>
+        				</div>
+      			</div>
+      			<div className={styles.askPermiso}>
+        				<img className={styles.askPermisoChild} alt="" src="Group 14.svg" />
+        				<div className={styles.scrollbar}>
+          					<div className={styles.dasplaycircle1} />
+        				</div>
+        				<img className={styles.fmdBadIcon1} alt="" src="fmd_bad.svg" />
+        				<img className={styles.askPermisoItem} alt="" src="Group 2608734.svg" />
+        				<div className={styles.permitirAHealthcheck}>¿Permitir a HealthCheck usar y almacenar tu ubicación?</div>
+          					<div className={styles.chip}>
+            						<div className={styles.chipChild} />
+            						<div className={styles.smallChip}>Allow</div>
+            						<img className={styles.iconscheck24px1} alt="" src="Icons/check_24px.svg" />
+          					</div>
+          					<div className={styles.chip1}>
+            						<div className={styles.chipChild} />
+            						<div className={styles.smallChip}>Decline</div>
+            						<img className={styles.iconscheck24px1} alt="" src="Icons/close_24px.svg" />
+          					</div>
+          					<div className={styles.title1} />
+          					</div>
+          					</div>);
+        				};
+        				
+        				export default Marketplace;
+        				
